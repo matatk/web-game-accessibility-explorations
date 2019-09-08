b1471c02c89c34e5773ffea8ab927fced363f5fb
Matthew Tylee Atkinson <matatk@gmail.com>
2019-09-08 14:42:02 +0100

    feat: Hello SDL
    Some weirdness here: can get SDL2 to work nicely on macOS, but really wanted to use SDL1 for simplicity (as emscripten has a built-in implementation of it). The blank initial window issue appears to look similar to the OpenGL issues on macOS Mojave, but as this is not using OpenGL...

---
+++ 00-hello-sdl-cpp/emtestsdl1.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <SDL.h>
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+extern "C" int main(int argc, char** argv) {
+	printf("Hello, world!\n");
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL initialisation error: %s\n", SDL_GetError());
+		return 42;
+	}
+
+	SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
+	if (screen == NULL) {
+		printf("SDL screen initialisation error: %s\n", SDL_GetError());
+		return 43;
+	}
+
+	SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0x80, 0));
+	SDL_Flip(screen);
+
+#ifndef __EMSCRIPTEN__
+	// macOS requires an event loop to display anything
+	// <found on StackOverflow>
+	SDL_Event e;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+			if (e.type == SDL_KEYDOWN) {
+				quit = true;
+			}
+			if (e.type == SDL_MOUSEBUTTONDOWN) {
+				quit = true;
+			}
+		}
+	}
+#endif
+
+	SDL_Quit();
+	return 0;
+}
