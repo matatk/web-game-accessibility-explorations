f07a62ef3200da2808cf91e8e7ac408339b14fc4
Matthew Tylee Atkinson <matatk@gmail.com>
2019-09-25 10:53:52 +0100

    Test renderer refactoring by adding a fancy printing renderer

---
+++ 02-menu/render/render_print_fancy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// FIXME use macro defn for padding
+
+#include "../menu/menu.h"
+
+void render_menu(Menu *menu) {
+	printf("---======<<<<<< MENU >>>>>>======---\n");
+	for (int i = 0; i < menu->length; i++) {
+		printf("|   %s %-22s %-3s   |\n",
+			i == menu->current ? "+" : " ",
+			menu->items[i]->name,
+			menu->items[i]->sub_menu != NULL ? ">>>" : "");
+	}
+	printf("---======<<<<<<~~~~~~>>>>>>======---\n");
+}
