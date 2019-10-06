27dcc5abb4a5c526815ba481333a4abc97dc3ca0
Matthew Tylee Atkinson <matatk@gmail.com>
2019-10-06 18:06:44 +0100

    Add script to install all deps and build all explorations

---
+++ install-and-build-all.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo 'This will carry out the install steps in the main README (except for Emscripten set-up, which needs to be done manually) and then install the dependencies of, and build, all explorations.' | fmt
+echo
+echo 'Interrupt to abort, or press ENTER to continue...'
+read -r
+
+echo Installing repo native dependencies...
+if [ "$(uname -s)" = 'Darwin' ]; then
+	brew install cppcheck
+else
+	sudo apt-get install cppcheck
+fi
+echo
+
+echo Installing repo Node dependencies...
+npm install
+echo
+
+echo Installing dependencies of, and building, each exploration...
+echo
+for makefile in */Makefile; do
+	DIR=$(dirname "$makefile")
+	cd "$DIR" || continue
+	echo "Exploration: $DIR"
+	if grep '^install-deps:' Makefile > /dev/null; then
+		make install-deps
+		echo
+	fi
+	make
+	cd - > /dev/null || exit
+	echo
+done
