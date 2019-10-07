cd23e0c752d24b3efdf83c62324c6c3ae945127f
Matthew Tylee Atkinson <matatk@gmail.com>
2019-10-07 09:13:05 +0100

    Simplify Makefile deps; Better header org
    * Not making HTML versions of CLI progs; remove deps.
    * As the functions to create menus are used by client code, move their definitions to the public header.
    * Also move the max menu length #define to the public header.

--- 04-menu-general/menu.h
+++ 04-menu-general/menu.h
@@ -1,7 +1,13 @@
 #ifndef _MENU_H_
 #define _MENU_H_
 
-#include "menu_p.h"
+typedef struct Menu Menu;
+typedef struct MenuItem MenuItem;
+
+#define MAX_ITEM_NAME_SIZE 23
+
+Menu *new_menu(int, ...);
+MenuItem *new_menu_item(const char *, Menu *);
 
 void menu_down(Menu *);
 void menu_up(Menu *);
@@ -9,5 +15,7 @@ Menu *menu_activate(Menu *);
 Menu *menu_back(Menu *);
 
 void debug_print_menu_tree(Menu *);
+
+#include "menu_p.h"
 
 #endif
