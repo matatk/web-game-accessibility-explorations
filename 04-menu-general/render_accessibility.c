fcdb4660c66563b5acec97eb79ba81045ff0fd09
Matthew Tylee Atkinson <matatk@gmail.com>
2019-11-26 11:12:48 +0000

    Put back in the widget name formatting, to tell them apart for now

--- 04-menu-general/render_accessibility.c
+++ 04-menu-general/render_accessibility.c
@@ -6,6 +6,7 @@
 #include "page.h"
 #include "widget.h"
 #include "widgetcontainer.h"
+#include "widgetslider.h"
 
 // Our custom JavaScript library functions
 extern void
@@ -21,29 +22,18 @@ accessibility_render_current_item(Page *, int);
 
 // State
 
-bool accessibility_setup_done = false;
+static bool accessibility_setup_done = false;
 
-// Internal (originally copied from render_sdl)
+// Internal
 
 static void render_widget(Page *page, int depth, Widget *widget);
 static bool in_horizontal_container = false;
 
 static void
 render_container_widget(Page *page, int depth, Widget *widget) {
-	/*
-	WidgetContainer *wc = AS_WIDGET_CONTAINER(widget);
-	char *string; // FIXME free (and DRY w' others)
-
-	in_horizontal_container = wc->orientation == HORIZONTAL;
-
-	if (wc->parent == NULL) {
-		asprintf(&string, "%s\n", wc->name);
-		render_container_header(screen, widget, string, depth);
-	}
-	*/
 	WidgetContainer *wc = AS_WIDGET_CONTAINER(widget);
 	in_horizontal_container = wc->orientation == HORIZONTAL;
-	//accessibility_render_page_item(item->name, widget_is_a(item, SUBPAGE));
+
 	accessibility_render_page_item(wc->name, false);
 
 	for (int i = 0; i < wc->length; i++) {
@@ -52,60 +42,39 @@ render_container_widget(Page *page, int depth, Widget *widget) {
 			in_horizontal_container ? depth : depth + 1,
 			wc->widgets[i]);
 	}
-
-	/*
-	if (in_horizontal_container) vertical_pos++;
-	in_horizontal_container = false;
-	horizontal_pos = 0;
-	*/
 }
 
 static void
 render_widgety_widget(Page *page, int depth, Widget *widget) {
-	char *string; // FIXME free (and DRY w' others)
-
-	// FIXME DRY with render_container_header
-	//SDL_Surface *rendered_widget = make_widget_surface(depth);
-
+	char *string;
 	switch (widget->type) {
 	case BUTTON:
 		asprintf(&string, "[%s]\n", widget->name);
-		//render_string(rendered_widget, page, widget, string);
 		accessibility_render_page_item(string, false);
 		break;
 	case SUBPAGE:
 		asprintf(&string, "%s >\n", widget->name);
-		//render_string(rendered_widget, page, widget, string);
 		accessibility_render_page_item(string, true);
 		break;
 	case LABEL:
 		asprintf(&string, "%s:\n", widget->name);
-		//render_string(rendered_widget, page, widget, string);
 		accessibility_render_page_item(string, false);
 		break;
 	case TEXTBOX:
-		//render_textbox(rendered_widget, page, widget);
+		asprintf(&string, "_%s_\n", widget->name);
 		accessibility_render_page_item(string, false);
 		break;
 	case SLIDER:
-		//render_slider(rendered_widget, page, AS_WIDGET_SLIDER(widget));
+		WidgetSlider *slider = AS_WIDGET_SLIDER(widget);
+		asprintf(&string, "~~%s (%d, %d, %d)~~\n", widget->name,
+			slider->value_min, slider->value, slider->value_max);
 		accessibility_render_page_item(string, false);
 		break;
 	default:
-		/*
 		asprintf(&string, "BASE WIDGET: %s\n", widget->name);
-		render_string(rendered_widget, page, widget, string);
-		*/
-		//render_string(rendered_widget, page, widget, widget->name);
 		accessibility_render_page_item(string, false);
 		break;
 	}
-
-	/*
-	SDL_Rect pos = get_pos_rect(depth);
-	SDL_BlitSurface(rendered_widget, NULL, screen, &pos);
-	SDL_FreeSurface(rendered_widget);
-	*/
 }
 
 static void
@@ -128,12 +97,6 @@ expose_page(Page *page) {
 
 	accessibility_render_page_start(page);
 
-	/*
-	for (int i = 0; i < page->length; i++) {
-		Widget *item = page->items[i];
-		accessibility_render_page_item(item->name, widget_is_a(item, SUBPAGE));
-	}
-	*/
 	Widget *root = page->root;
 	render_widget(page, 0, root);
 
